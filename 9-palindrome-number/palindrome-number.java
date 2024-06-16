445ba1ac5a4526b8c9c05f3a4fe7d98245b9aa96
Brandon Leho <89223038+BrandonLeho@users.noreply.github.com>
2024-06-16 00:08:26 -0700

    Time: 6 ms (37.01%) | Memory: 43.9 MB (38.58%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isPalindrome(int x) {
         String input = Integer.toString(x);
         int n = String.valueOf(x).length() - 1;
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n/2 + 1; i++){
             if(input.charAt(i) != input.charAt(n - i)){
                 return false;
             }
