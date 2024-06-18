a8e47d8c073c3603f2927ed47afbd18d38c239f1
Brandon Leho <89223038+BrandonLeho@users.noreply.github.com>
2024-06-17 17:41:11 -0700

    Time: 17 ms (86.61%) | Memory: 45.6 MB (49.14%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.java
+++ 853-most-profit-assigning-work/most-profit-assigning-work.java
@@ -1,24 +1,26 @@
 class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        int output = 0, counter = 0, max = 0;
-        int[] potentialProjects = new int[difficulty.length];
+        int n = difficulty.length;
+        Integer[] job = new Integer[n];
 
-        for(int i = 0; i < worker.length; i++){
-            for(int j = 0; j < difficulty.length; j++){
-                if(worker[i] >= difficulty[j]){
-                    potentialProjects[counter] = profit[j];
-                    counter++;
-                }
+        for (int i = 0; i < n; i++) {
+            job[i] = i;
+        }
+
+        Arrays.sort(job, (a, b) -> difficulty[a] - difficulty[b]);
+        Arrays.sort(worker);
+
+        int maxProfit = 0;
+        int i = 0;
+        int best = 0;
+        int output = 0;
+
+        for (int w : worker) {
+            while (i < n && w >= difficulty[job[i]]) {
+                best = Math.max(best, profit[job[i]]);
+                i++;
             }
-            for(int j = 0; j < potentialProjects.length; j++){
-                if(potentialProjects[j] >= max){
-                    max = potentialProjects[j];
-                }
-            }
-            output += max;
-            max = 0;
-            counter = 0;
-            Arrays.fill(potentialProjects, 0);
+            output += best;
         }
         return output;
     }
