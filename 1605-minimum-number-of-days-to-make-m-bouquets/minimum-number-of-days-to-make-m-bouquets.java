c14b49995c42fb42b654ca2ebf8579e5896386f1
Brandon Leho <89223038+BrandonLeho@users.noreply.github.com>
2024-06-18 20:47:15 -0700

    Time: 19 ms (67.64%) | Memory: 58.7 MB (11.51%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,41 +1,43 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
-        int n = bloomDay.length;
-        if ((long)m * k > n)
+        if ((long) m * k > bloomDay.length) {
             return -1;
-
-        int left = Arrays.stream(bloomDay).min().getAsInt();
-        int right = Arrays.stream(bloomDay).max().getAsInt();
-
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (canMakeBouquets(bloomDay, m, k, mid)) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
         }
-        return left;
+
+        int low = 1, high = (int) 1e9;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (isPossibleBouquets(bloomDay, m, k, mid)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        };
+
+        return low;
     }
+    private boolean isPossibleBouquets(int[] bloomDay, int m, int k, int day) {
+        int total = 0;
 
-    private boolean canMakeBouquets(int[] bloomDay, int m, int k, int day) {
-        int bouquets = 0;
-        int flowers = 0;
-
-        for (int bloom : bloomDay) {
-            if (bloom <= day) {
-                flowers++;
-                if (flowers == k) {
-                    bouquets++;
-                    flowers = 0;
-                }
-            } else {
-                flowers = 0;
+        for (int i = 0; i < bloomDay.length; i++) {
+            int count = 0;
+            while (i < bloomDay.length && count < k && bloomDay[i] <= day) {
+                count++;
+                i++;
             }
 
-            if (bouquets >= m)
+            if (count == k) {
+                total++;
+                i--;
+            }
+
+            if (total >= m) {
                 return true;
+            }
         }
+
         return false;
     }
+
 }
