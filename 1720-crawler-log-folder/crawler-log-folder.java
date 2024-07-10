6de5d8dccc30c6009861135005515153038d542d
Brandon Leho <89223038+BrandonLeho@users.noreply.github.com>
2024-07-10 12:17:25 -0700

    Time: 1 ms (99.42%) | Memory: 42 MB (51.62%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.java
+++ 1720-crawler-log-folder/crawler-log-folder.java
@@ -1,12 +1,11 @@
 class Solution {
     public int minOperations(String[] logs) {
         int output = 0;
-        for(int i = 0; i < logs.length; i++){
+        for(int i = 0; i < logs.length; i++)
             if(!logs[i].equals("../") && !logs[i].equals("./"))
                 output++;
             else if(logs[i].equals("../") && i != 0 && output > 0)
                 output--;
-        }
         return output;
     }
 }
