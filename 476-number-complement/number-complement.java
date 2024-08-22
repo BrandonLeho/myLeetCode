d5082620a80bba9d4dca0fc9b3ee86a0cdc088f4
Brandon Leho <89223038+BrandonLeho@users.noreply.github.com>
2024-08-22 09:45:35 -0700

    Time: 0 ms (100.00%) | Memory: 40.6 MB (20.00%) - LeetSync

--- 476-number-complement/number-complement.java
+++ 476-number-complement/number-complement.java
@@ -1,17 +1,8 @@
 class Solution {
     public int findComplement(int num) {
-        String binaryString = Integer.toBinaryString(num);
-        char[] binaryChar = binaryString.toCharArray();
-        for(int i = 0; i < binaryChar.length; i++){
-            if(binaryChar[i] == '0'){
-                binaryChar[i] = '1';
-            }
-            else{
-                binaryChar[i] = '0';
-            }
-        }
-        String modifiedBinaryString = new String(binaryChar);
-        long binaryInt = Long.parseLong(modifiedBinaryString, 2);
-        return (int)(binaryInt);
+        if (num == 0) return 1;
+        int bitLength = Integer.toBinaryString(num).length();
+        int mask = (1 << bitLength) - 1;
+        return num ^ mask;
     }
 }
