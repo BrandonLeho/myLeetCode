3801662ac860bbecdcb969ed687012658e4edbc7
Brandon Leho <89223038+BrandonLeho@users.noreply.github.com>
2024-09-02 09:59:24 -0700

    Time: 2 ms (72.59%) | Memory: 59.8 MB (60.59%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.java
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.java
@@ -1,13 +1,20 @@
+
 class Solution {
-    public int chalkReplacer(int[] chalk, int k) {
-        if(chalk.length == 1) return 0;
-        int n = 0;
-        while(k > 0){
-            k -= chalk[n];
-            if(k < 0) return n;
-            n++;
-            if(n > chalk.length - 1) n = 0;
+    public int chalkReplacer(int[] chalk, int initialChalkPieces) {
+        long totalChalkNeeded = 0;
+        for (int studentChalkUse : chalk) {
+            totalChalkNeeded += studentChalkUse;
         }
-        return n;
+        
+        int remainingChalk = (int)(initialChalkPieces % totalChalkNeeded);
+        
+        for (int studentIndex = 0; studentIndex < chalk.length; studentIndex++) {
+            if (remainingChalk < chalk[studentIndex]) {
+                return studentIndex;
+            }
+            remainingChalk -= chalk[studentIndex];
+        }
+        
+        return 0;  
     }
 }
