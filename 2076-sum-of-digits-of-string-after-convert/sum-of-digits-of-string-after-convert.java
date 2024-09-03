204a089a27b4b9e02762dfce6e08f6ce9c22cf88
Brandon Leho <89223038+BrandonLeho@users.noreply.github.com>
2024-09-03 10:32:16 -0700

    Time: 0 ms (100.00%) | Memory: 41.1 MB (98.21%) - LeetSync

--- 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.java
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.java
@@ -5,14 +5,14 @@ class Solution {
             int convert = (int)(s.charAt(i)) - 96;
             sum += convert / 10;
             sum += convert % 10;
-        } //595 360
+        }
         if(k > 1){
             for(int i = 1; i < k; i++){
                 int tempSum = 0;
-                tempSum += (sum / 1000) % 10;
-                tempSum += (sum / 100) % 10;
-                tempSum += (sum / 10) % 10;
-                tempSum += sum % 10;
+                while (sum != 0) {
+                    tempSum += sum % 10;
+                    sum /= 10;
+                }
                 sum = tempSum;
             }
         }
