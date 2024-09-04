c1dd357e805805253d1b7dc117964dae4680d2cb
Brandon Leho <89223038+BrandonLeho@users.noreply.github.com>
2024-09-04 12:08:38 -0700

    Time: 39 ms (25.33%) | Memory: 48.9 MB (85.59%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.java
+++ 906-walking-robot-simulation/walking-robot-simulation.java
@@ -1,34 +1,62 @@
 class Solution {
     public int robotSim(int[] commands, int[][] obstacles) {
         int distance = 0, x = 0, y = 0, direction = 0;
-        int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
         
         Set<String> obstacleSet = new HashSet<>();
         for (int[] obstacle : obstacles) {
             obstacleSet.add(obstacle[0] + "," + obstacle[1]);
         }
 
-        for (int command : commands) {
-            if (command == -1) {
-                direction = (direction + 1) % 4;
-            } else if (command == -2) {
-                direction = (direction + 3) % 4;
-            } else {
-                int dx = dirs[direction][0];
-                int dy = dirs[direction][1];
-                for (int k = 0; k < command; k++) {
-                    int nx = x + dx;
-                    int ny = y + dy;
-                    if (obstacleSet.contains(nx + "," + ny)) {
+        for (int i = 0; i < commands.length; i++) {
+            if (commands[i] == -1) {  // turn right
+                direction++;
+                if (direction == 4) direction = 0;
+            } else if (commands[i] == -2) {  // turn left
+                direction--;
+                if (direction == -1) direction = 3;
+            } else {  // move forward
+                switchLoop:
+                switch (direction) {
+                    case 0: // north
+                        for (int step = 1; step <= commands[i]; step++) {
+                            if (obstacleSet.contains(x + "," + (y + step))) {
+                                y += (step - 1);
+                                break switchLoop;
+                            }
+                        }
+                        y += commands[i];
                         break;
-                    }
-                    x = nx;
-                    y = ny;
-                    distance = Math.max(distance, x * x + y * y);
+                    case 1: // east
+                        for (int step = 1; step <= commands[i]; step++) {
+                            if (obstacleSet.contains((x + step) + "," + y)) {
+                                x += (step - 1);
+                                break switchLoop;
+                            }
+                        }
+                        x += commands[i];
+                        break;
+                    case 2: // south
+                        for (int step = 1; step <= commands[i]; step++) {
+                            if (obstacleSet.contains(x + "," + (y - step))) {
+                                y -= (step - 1);
+                                break switchLoop;
+                            }
+                        }
+                        y -= commands[i];
+                        break;
+                    case 3: // west
+                        for (int step = 1; step <= commands[i]; step++) {
+                            if (obstacleSet.contains((x - step) + "," + y)) {
+                                x -= (step - 1);
+                                break switchLoop;
+                            }
+                        }
+                        x -= commands[i];
+                        break;
                 }
+                distance = Math.max(distance, x * x + y * y);
             }
         }
-
         return distance;
     }
 }
