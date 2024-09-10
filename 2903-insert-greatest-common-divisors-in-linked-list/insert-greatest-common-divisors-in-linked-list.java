0749727343aee841ed7134cc3d9ae594367e1c55
Brandon Leho <89223038+BrandonLeho@users.noreply.github.com>
2024-09-10 10:46:40 -0700

    Time: 2 ms (71.11%) | Memory: 44.9 MB (61.64%) - LeetSync

--- 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.java
+++ 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.java
@@ -10,36 +10,14 @@
  */
 class Solution {
     public ListNode insertGreatestCommonDivisors(ListNode head) {
-        ListNode current = head, c = head;
-        int position = 1, n = 0;
-
-        while (c != null) {
-            n++;
-            c = c.next;
-        }
-
-        for(int i = 0; i < n - 1; i++){
-            int gcd = findGCD(current.val, current.next.val);
-            head = insertAtPosition(head, gcd, position);
-            position += 2;
-            current = current.next.next;
-        }
-        return head;
-    }
-
-    public ListNode insertAtPosition(ListNode head, int value, int position) {
-        ListNode newNode = new ListNode(value);
         ListNode current = head;
-        int currentIndex = 0;
 
-        while (current != null && currentIndex < position - 1) {
-            current = current.next;
-            currentIndex++;
-        }
-
-        if (current != null) {
-            newNode.next = current.next;
-            current.next = newNode;
+        while (current != null && current.next != null) {
+            int gcd = findGCD(current.val, current.next.val);
+            ListNode gcdNode = new ListNode(gcd);
+            gcdNode.next = current.next;
+            current.next = gcdNode;
+            current = gcdNode.next;
         }
 
         return head;
