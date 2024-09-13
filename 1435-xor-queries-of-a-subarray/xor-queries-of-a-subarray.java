622551f835c36ebc32a138e8a02e2a525adfbfdd
Brandon Leho <89223038+BrandonLeho@users.noreply.github.com>
2024-09-13 10:45:25 -0700

    Time: 543 ms (21.65%) | Memory: 55.9 MB (80.73%) - LeetSync

---
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
@@ -0,0 +1,21 @@
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
+
+class Solution {
+    public int[] xorQueries(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            if (queries[i][0] == queries[i][1]) {
+                answer[i] = arr[queries[i][0]];
+            } else {
+                int binary = 0;
+                for (int j = queries[i][0]; j <= queries[i][1]; j++) {
+                    binary ^= arr[j];
+                }
+                answer[i] = binary;
+            }
+        }
+        return answer;
+    }
+}
