6dc04b907c1e4848438c5a1288ce6909d27f5ebf
Brandon Leho <89223038+BrandonLeho@users.noreply.github.com>
2024-09-14 10:44:09 -0700

    Time: 2 ms (100.00%) | Memory: 59.2 MB (69.66%) - LeetSync

--- 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.java
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.java
@@ -1,20 +1,27 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-        int output = 1, max = 0, counter = 1;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] > max){
-                max = nums[i];
-                output = 1;
-                counter = 1;
-            }
-            else if(nums[i] == nums[i-1] && nums[i] == max){
-                counter++;
-            }
-            else{
-                counter = 1;
-            }
-            output = Math.max(counter, output);
+        int len = 1;
+
+        // Step 1: Find the maximum element in the array
+        int mx = Integer.MIN_VALUE;
+        for (int num : nums) {
+            mx = Math.max(mx, num);  // O(n)
         }
-        return output;
+
+        // Step 2: Find the longest subarray consisting of the maximum element
+        int tmp_len = 0;
+        for (int num : nums) {  // O(n)
+            if (num == mx) {
+                tmp_len++;
+            } else {
+                len = Math.max(len, tmp_len);
+                tmp_len = 0;
+            }
+        }
+
+        // Update len in case the longest subarray is at the end
+        len = Math.max(len, tmp_len);
+
+        return len;
     }
 }
