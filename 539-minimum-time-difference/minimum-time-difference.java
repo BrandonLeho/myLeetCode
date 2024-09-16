9384b2744152f2e38f4976ea9317943a6a10f790
Brandon Leho <89223038+BrandonLeho@users.noreply.github.com>
2024-09-16 13:27:36 -0700

    Time: 5 ms (67.09%) | Memory: 45.5 MB (50.25%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.java
+++ 539-minimum-time-difference/minimum-time-difference.java
@@ -1,34 +1,24 @@
 class Solution {
+
     public int findMinDifference(List<String> timePoints) {
-        int min = Integer.MAX_VALUE;
-        Collections.sort(timePoints);
+        int[] minutes = new int[timePoints.size()];
         for (int i = 0; i < timePoints.size(); i++) {
-            String time1 = timePoints.get(i);
-            String time2 = "";
-            if (i == timePoints.size() - 1) {
-                time2 = timePoints.get(0);
-            } else {
-                time2 = timePoints.get(i + 1);
-            }
-
-            if (time1.equals(time2)) {
-                return 0;
-            }
-
-            int hour1 = Integer.parseInt(time1.substring(0, 2));
-            int hour2 = Integer.parseInt(time2.substring(0, 2));
-            int min1 = Integer.parseInt(time1.substring(3, 5));
-            int min2 = Integer.parseInt(time2.substring(3, 5));
-            int timeDif = Math.abs((hour1 * 60 + min1) - (hour2 * 60 + min2));
-
-            if (i == timePoints.size() - 1) {
-                hour1 = 23 - hour1;
-                min1 = 60 - min1;
-                timeDif = ((hour1 + hour2) * 60) + (min1 + min2);
-            }
-
-            min = Math.min(timeDif, min);
+            String time = timePoints.get(i);
+            int h = Integer.parseInt(time.substring(0, 2));
+            int m = Integer.parseInt(time.substring(3));
+            minutes[i] = h * 60 + m;
         }
-        return min;
+
+        Arrays.sort(minutes);
+
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < minutes.length - 1; i++) {
+            ans = Math.min(ans, minutes[i + 1] - minutes[i]);
+        }
+
+        return Math.min(
+            ans,
+            24 * 60 - minutes[minutes.length - 1] + minutes[0]
+        );
     }
 }
