10f69622d4f132002fb79b426f4d9d73eb10b4f3
Brandon Leho <89223038+BrandonLeho@users.noreply.github.com>
2024-09-16 13:26:21 -0700

    Time: 12 ms (32.16%) | Memory: 45.4 MB (55.15%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.java
+++ 539-minimum-time-difference/minimum-time-difference.java
@@ -2,16 +2,18 @@ class Solution {
     public int findMinDifference(List<String> timePoints) {
         int min = Integer.MAX_VALUE;
         Collections.sort(timePoints);
-        for(int i = 0; i < timePoints.size(); i++){
+        for (int i = 0; i < timePoints.size(); i++) {
             String time1 = timePoints.get(i);
             String time2 = "";
-            if(i == timePoints.size() - 1){
+            if (i == timePoints.size() - 1) {
                 time2 = timePoints.get(0);
-            }else{
-                time2 = timePoints.get(i+1);
+            } else {
+                time2 = timePoints.get(i + 1);
             }
-            
-            if(time1.equals(time2)){return 0;}
+
+            if (time1.equals(time2)) {
+                return 0;
+            }
 
             int hour1 = Integer.parseInt(time1.substring(0, 2));
             int hour2 = Integer.parseInt(time2.substring(0, 2));
@@ -19,12 +21,12 @@ class Solution {
             int min2 = Integer.parseInt(time2.substring(3, 5));
             int timeDif = Math.abs((hour1 * 60 + min1) - (hour2 * 60 + min2));
 
-            if(i == timePoints.size() - 1){
+            if (i == timePoints.size() - 1) {
                 hour1 = 23 - hour1;
                 min1 = 60 - min1;
                 timeDif = ((hour1 + hour2) * 60) + (min1 + min2);
             }
-            
+
             min = Math.min(timeDif, min);
         }
         return min;
