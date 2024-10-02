1b4d131e67a996c1751098415e5781e0b9e59acb
Brandon Leho <89223038+BrandonLeho@users.noreply.github.com>
2024-10-02 10:45:25 -0700

    Time: 26 ms (69.92%) | Memory: 62.3 MB (45.54%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -1,18 +1,24 @@
-import java.util.*;
-
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        Map<Integer, Integer> valueToRank = new HashMap<>();
-        int[] sortedUniqueNumbers = Arrays.stream(arr).distinct().sorted().toArray(); 
+        Map<Integer, Integer> hm = new HashMap<Integer, Integer>();
+        int temp[] = Arrays.copyOf(arr, arr.length);
+        Arrays.sort(temp); 
+
+        if(arr.length==0){
+            return new int[]{};
+        }
         
-        for (int i = 0; i < sortedUniqueNumbers.length; i++) {
-            valueToRank.put(sortedUniqueNumbers[i], i + 1);
+        hm.put(temp[0], 1);
+
+        for (int i = 1; i < temp.length; i++) {
+            if (temp[i] > temp[i - 1]) {
+                hm.put(temp[i], hm.get(temp[i - 1]) + 1);
+            }
         }
 
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = valueToRank.get(arr[i]);
+            arr[i] = hm.get(arr[i]);
         }
-
         return arr;
     }
 }
