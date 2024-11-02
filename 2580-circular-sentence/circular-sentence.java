a8566007c4f437d2e5f9eaf519a36b0cd6fe7897
Brandon Leho <89223038+BrandonLeho@users.noreply.github.com>
2024-11-02 06:05:50 -0700

    Time: 2 ms (18.09%) | Memory: 42 MB (54.87%) - LeetSync

--- 2580-circular-sentence/circular-sentence.java
+++ 2580-circular-sentence/circular-sentence.java
@@ -1,14 +1,9 @@
 class Solution {
     public boolean isCircularSentence(String sentence) {
-        if(sentence.charAt(sentence.length() - 1) != sentence.charAt(0)){ return false; }
-
-        for(int i = 0; i < sentence.length(); i++){
-            char c =  sentence.charAt(i);
-            if(c == ' ' && i > 0 && i < sentence.length()){
-                if(sentence.charAt(i - 1) != sentence.charAt(i + 1)){
-                    return false;
-                }
-            }
+        String[] arr = sentence.split(" ");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].charAt(arr[i].length() - 1) != arr[(i + 1) % arr.length].charAt(0))
+                return false;
         }
         return true;
     }
