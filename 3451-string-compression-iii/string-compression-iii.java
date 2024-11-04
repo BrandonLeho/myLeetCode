4fbffe5a47a17aa714e3dc4aaa49fc172c65b68c
Brandon Leho <89223038+BrandonLeho@users.noreply.github.com>
2024-11-04 10:44:49 -0800

    Time: 16 ms (94.92%) | Memory: 45.7 MB (68.96%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.java
+++ 3451-string-compression-iii/string-compression-iii.java
@@ -1,34 +1,29 @@
 class Solution {
     public String compressedString(String word) {
-        List<Character> compList = new ArrayList<>();
         StringBuilder comp = new StringBuilder();
         int count = 1;
         char c = word.charAt(0);
 
         for (int i = 1; i < word.length(); i++) {
-            char curr = word.charAt(i);
+            char curr = word.charAt(i), num = (char)(count + '0');
             if (c == curr) {
                 if (count > 8) {
-                    compList.add((char)(count + '0'));
-                    compList.add(c);
+                    comp.append(num);
+                    comp.append(c);
                     count = 0;
                 }
                 count++;
 
             } else {
-                compList.add((char)(count + '0'));
-                compList.add(c);
+                comp.append(num);
+                comp.append(c);
                 count = 1;
                 c = curr;
             }
 
         }
-        compList.add((char)(count + '0'));
-        compList.add(c);
-
-        for (char ch : compList) {
-            comp.append(ch);
-        }
+        comp.append((char)(count + '0'));
+        comp.append(c);
 
         return comp.toString();
     }
