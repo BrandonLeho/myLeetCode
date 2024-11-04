2706350d1ba9090da061d9c20c5f903a0bf0e2bb
Brandon Leho <89223038+BrandonLeho@users.noreply.github.com>
2024-11-04 10:47:43 -0800

    Time: 16 ms (94.92%) | Memory: 45.7 MB (68.96%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.java
+++ 3451-string-compression-iii/string-compression-iii.java
@@ -6,23 +6,16 @@ class Solution {
 
         for (int i = 1; i < word.length(); i++) {
             char curr = word.charAt(i), num = (char) (count + '0');
-            if (c == curr) {
-                if (count > 8) {
-                    comp.append(num);
-                    comp.append(c);
-                    count = 0;
-                }
+            if (c == curr && count < 9) {
                 count++;
             } else {
-                comp.append(num);
-                comp.append(c);
+                comp.append(num).append(c);
                 count = 1;
                 c = curr;
             }
 
         }
-        comp.append((char) (count + '0'));
-        comp.append(c);
+        comp.append((char) (count + '0')).append(c);
 
         return comp.toString();
     }
