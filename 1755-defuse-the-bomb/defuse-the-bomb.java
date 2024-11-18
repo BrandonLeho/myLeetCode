2609a59e31c60149c68906ee173728425d2ac9ba
Brandon Leho <89223038+BrandonLeho@users.noreply.github.com>
2024-11-18 09:26:43 -0800

    Time: 3 ms (14.82%) | Memory: 42.8 MB (8.63%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.java
+++ 1755-defuse-the-bomb/defuse-the-bomb.java
@@ -1,29 +1,20 @@
 class Solution {
     public int[] decrypt(int[] code, int k) {
-        int[] decode = new int[code.length];
-        if(k == 0)
-            return decode;
-
-        int[] wrap = new int[code.length * 2];
-        int mid = wrap.length / 2;
-        for(int i = 0; i < wrap.length; i++){
-            if(i < mid)
-                wrap[i] = code[i];
-            else
-                wrap[i] = wrap[i - mid];
-        }
+        int N = code.length;
+        int[] res = new int[N];
         
-        
-        for(int i = 0; i < decode.length; i++){
-            for(int j = 0; j < Math.abs(k); j++){
-                if(k < 0){
-                    decode[i] += wrap[mid - 1 + i - j];
-                } else {
-                    decode[i] += wrap[i + j + 1];
+        for (int i = 0; i < N; i++) {
+            if (k > 0) {
+                for (int j = i + 1; j < i + 1 + k; j++) {
+                    res[i] += code[j % N];
+                }
+            } else if (k < 0) {
+                for (int j = i - 1; j > i - 1 - Math.abs(k); j--) {
+                    res[i] += code[((j % N) + N) % N];
                 }
             }
         }
-
-        return decode;
+        
+        return res;
     }
 }
