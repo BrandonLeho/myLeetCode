2440561162fe114964c3bbb5811c653037f6e542
Brandon Leho <89223038+BrandonLeho@users.noreply.github.com>
2024-12-12 09:50:43 -0800

    Time: 5 ms (89.83%) | Memory: 42.6 MB (40.26%) - LeetSync

--- 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.java
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.java
@@ -2,15 +2,15 @@ class Solution {
     public long pickGifts(int[] gifts, int k) {
         long sum = 0;
         PriorityQueue<Integer> sortedGifts = new PriorityQueue<>(Comparator.reverseOrder());
-        Arrays.stream(gifts).boxed().forEach(sortedGifts::add);
 
-        for(int i = 0; i < k; i++){
-            sortedGifts.add((int)Math.sqrt(sortedGifts.poll()));
-        }
+        for (int n : gifts)
+            sortedGifts.add(n);
 
-        for(int n : sortedGifts){
+        for (int i = 0; i < k; i++)
+            sortedGifts.add((int) Math.sqrt(sortedGifts.poll()));
+
+        for (int n : sortedGifts)
             sum += n;
-        }
 
         return sum;
     }
