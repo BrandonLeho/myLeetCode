8a84e6d4b7c42b2c13a701c23621997655b7ec74
Brandon Leho <89223038+BrandonLeho@users.noreply.github.com>
2025-02-01 10:27:14 -0800

    Time: 0 ms (100.00%) | Memory: 43.2 MB (84.31%) - LeetSync

--- 3429-special-array-i/special-array-i.java
+++ 3429-special-array-i/special-array-i.java
@@ -1,18 +1,7 @@
 class Solution {
     public boolean isArraySpecial(int[] nums) {
-        if(nums.length == 1) return true;
-
-        int parity = 0;
-        if(nums[0] % 2 != 0) parity = 1;
-
-        for(int i = 1; i < nums.length; i++){
-            if(parity == 0 && nums[i] % 2 != 0){
-                parity = 1;
-            }
-            else if(parity == 1 && nums[i] % 2 == 0){
-                parity = 0;
-            }
-            else{
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] % 2 == nums[i + 1] % 2) {
                 return false;
             }
         }
