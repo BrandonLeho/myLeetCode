e79cfb333519fcac91f7e89a3b4c88b255892b35
Brandon Leho <89223038+BrandonLeho@users.noreply.github.com>
2025-07-01 09:38:21 -0700

    Time: 1 ms (87.19%) | Memory: 42.3 MB (38.84%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.java
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.java
@@ -1,13 +1,9 @@
 class Solution {
     public int possibleStringCount(String word) {
-        if (word.length() == 1)
-            return 1;
-
         int count = 0;
-        for (int i = 1; i < word.length(); i++) {
-            if (word.charAt(i) == word.charAt(i-1))
+        for (int i = 1; i < word.length(); i++)
+            if (word.charAt(i) == word.charAt(i - 1))
                 count++;
-        }
 
         return count + 1;
     }
